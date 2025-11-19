ca24ed0e57b21a9f286cd864b904b56dc38f6372
Captain CP <captain@barrersoftware.com>
2025-11-19 04:48:51 +0000

    Add Barrer branding with NetBird attribution
    Added product constants with clear NetBird attribution:
    - ProductName: Barrer Security Mesh VPN
    - ProductURL: Our GitHub repo
    - UpstreamName: NetBird
    - UpstreamURL: NetBird GitHub repo
    - Attribution: Built from NetBird Open Source Project
    
    Kept NetbirdVersion() for backward compatibility
    Added BarrerVersion() and GetProductInfo() for our branding
    
    Legal compliance: Attribution in code as required

--- version/version.go
+++ version/version.go
@@ -1,20 +1,40 @@
 package version
 
 import (
-	"regexp"
+"regexp"
 
-	v "github.com/hashicorp/go-version"
+v "github.com/hashicorp/go-version"
 )
 
 // will be replaced with the release version when using goreleaser
 var version = "development"
 
 var (
-	VersionRegexp = regexp.MustCompile("^" + v.VersionRegexpRaw + "$")
-	SemverRegexp  = regexp.MustCompile("^" + v.SemverRegexpRaw + "$")
+VersionRegexp = regexp.MustCompile("^" + v.VersionRegexpRaw + "$")
+SemverRegexp  = regexp.MustCompile("^" + v.SemverRegexpRaw + "$")
+)
+
+// Product information
+const (
+ProductName    = "Barrer Security Mesh VPN"
+ProductURL     = "https://github.com/BarrerSoftware/barrer-security-mesh-vpn"
+UpstreamName   = "NetBird"
+UpstreamURL    = "https://github.com/netbirdio/netbird"
+Attribution    = "Built from NetBird Open Source Project"
 )
 
 // NetbirdVersion returns the Netbird version
+// Kept for backward compatibility with original codebase
 func NetbirdVersion() string {
-	return version
+return version
+}
+
+// BarrerVersion returns the Barrer Security Mesh VPN version
+func BarrerVersion() string {
+return version
+}
+
+// GetProductInfo returns product name and attribution
+func GetProductInfo() (product, upstream, attribution string) {
+return ProductName, UpstreamName + " (" + UpstreamURL + ")", Attribution
 }
